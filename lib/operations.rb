5142a66e5868a28d068048084c4213c7f623636f
Meg Galvez <meg@oakmaine.com>
2019-02-16 21:24:19 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,13 @@
 def unsafe?(speed)
-
+  if speed < 40 || if speed > 60 
+    return true 
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 40 ? true : false || speed > 60 ? true : false
 end
 	
-
+ 
 
